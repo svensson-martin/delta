f46cd87413f03f32dd9a6e2b7badfe9d6332b17a
svensson-martin <svensson@imada.sdu.dk>
2022-07-04 20:28:44 +0200

    Added tests

--- R/cleaning-helpers.R
+++ R/cleaning-helpers.R
@@ -1,11 +1,21 @@
-
+#' Find whether the beach is in US or UK
+#'
+#' @param dat A data frame listing beaches
+#'
+#' @return A data frame with a column indicating US or UK
 #' @export
 localize_beach <- function(dat) {
   dplyr::left_join(dat, lookup_table)
 }
 
+
 f_to_c <- function(x) (x - 32) * 5 / 9
 
+#' Modifies temp to Celcius if country is US
+#'
+#' @param dat A data frame with countries and temperatures
+#'
+#' @return A data frame where the temperatures is in Farenheit
 #' @export
 celsify_temp <- function(dat) {
   dplyr::mutate(dat, temp = dplyr::if_else(english == "US", f_to_c(temp), temp))
@@ -17,6 +27,11 @@ timestamp <- function(time = Sys.time()) {
   format(time, "%Y-%B-%d_%H-%M-%S")
 }
 
+#' Adds a time stamp on a file
+#'
+#' @param infile A csv file
+#'
+#' @return A csv file with time stamp in title
 #' @export
 outfile_path <- function(infile) {
   ts <- timestamp(Sys.time())
